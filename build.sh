eb1d88a655d3246f5ab21256dabfdc306c109d10
j0hann3s <30905949+j0hann3s@users.noreply.github.com>
2024-12-20 11:17:19 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,11 +12,11 @@ RELEASE="$(rpm -E %fedora)"
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
+# bootc problem with selinux labels (swtpm for example)
+touch /.autorelabel
+
 # prepare hashicorp repo.
 curl https://rpm.releases.hashicorp.com/fedora/hashicorp.repo > /etc/yum.repos.d/hashicorp.repo
+
 # this installs a package from fedora repos
 rpm-ostree install ansible htop libguestfs p7zip python3-psutil qemu-kvm setroubleshoot terraform virt-manager virt-viewer
-
-# bootc problem with selinux labels (swtpm for example)
-systemctl start selinux-autorelabel.service
-touch /.autorelabel
